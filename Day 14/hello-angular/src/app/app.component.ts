ba724307fbb421c2403d71591f62c58944a583dc
Avri Bazar <76949502+fableDude@users.noreply.github.com>
2021-05-25 15:38:11 +0300

    ex7-calc angular

---
+++ Day 14/hello-angular/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  //data
+    num1: number = 0;
+    num2: number = 0;
+    calculated: boolean = false;
+    results: string[] = [];
+  //methods
+  setnum1(num:string){
+    this.num1=Number(num);
+  }
+
+  setnum2(num:string){
+    this.num2=Number(num);
+  }
+  calc():void{
+    this.calculated = true;
+    this.results = [
+        `${this.num1}+${this.num2}=${this.add()}`,
+        `${this.num1}-${this.num2}=${this.dif()}`,
+        `${this.num1}*${this.num2}=${this.mul()}`
+    ]
+  }
+  add():number{
+    return this.num1+this.num2;
+  }
+  dif():number{
+    return this.num1-this.num2;
+  }
+  mul():number{
+    return this.num1*this.num2;
+  }
+
+  
+}
